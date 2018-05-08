9ed78e9c39eac989634847d09742b480d7c2dbec
fandyrahmats <fanidayam@gmail.com>
2018-05-08 21:36:34 +0800

    adding skill activity

--- app/src/main/java/com/example/fandyrahmatsaputra/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/fandyrahmatsaputra/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.example.fandyrahmatsaputra.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun intentSkillActivityClicked(view: View) {
+        val skillIntent = Intent(this, SkillActivity::class.java)
+        startActivity(skillIntent)
+    }
+
 }
--- app/src/main/java/com/example/fandyrahmatsaputra/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/fandyrahmatsaputra/swoosh/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.fandyrahmatsaputra.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btn_intent_league.setOnClickListener {
+           val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
